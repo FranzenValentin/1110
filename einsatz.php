f53d28e2d6d4a9f5ba94c2f4c13b6e477ce9ca3c
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2024-12-25 19:37:23 +0100

    uhrzeit validierung raus

--- einsatz.php
+++ einsatz.php
@@ -43,13 +43,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 throw new Exception("Ungültige Fahrzeug-ID ausgewählt.");
             }
 
-            // Eingabedaten validieren
-            if (!preg_match('/\d{2}.\d{2}.\d{4} \d{2}:\d{2}', $alarmuhrzeit)) {
-                throw new Exception("Alarmuhrzeit hat ein ungültiges Format.");
-            }
-            if (!preg_match('/\d{2}.\d{2}.\d{4} \d{2}:\d{2}', $zurueckzeit)) {
-                throw new Exception("Zurückzeit hat ein ungültiges Format.");
-            }
 
             // Aktuellste Besatzung abrufen
             $stmt = $pdo->prepare("SELECT id FROM Besatzung ORDER BY id DESC LIMIT 1");
