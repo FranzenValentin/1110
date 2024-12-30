4be8eb10cb889a3a16eb505b40f807fcf0c2dd61
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2024-12-30 19:34:27 +0100

    namen jetzt alphabetich

--- besatzung.php
+++ besatzung.php
@@ -121,10 +121,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 echo "<td><em>Keine Zuweisung</em></td>";
                             }
 
-                            // Dropdown zur Auswahl
+                            // Dropdown zur Auswahl, nach Alphabet sortiert
                             echo "<td><select name='$key'>";
                             echo "<option value=''>Keine Auswahl</option>";
-                            $stmt = $pdo->query("SELECT id, CONCAT(vorname, ' ', nachname) AS name FROM Personal");
+                            $stmt = $pdo->query("SELECT id, CONCAT(vorname, ' ', nachname) AS name FROM Personal ORDER BY nachname, vorname ASC");
                             while ($row = $stmt->fetch()) {
                                 $selected = ($latestBesatzung && $latestBesatzung[$key . '_id'] == $row['id']) ? 'selected' : '';
                                 echo "<option value='{$row['id']}' $selected>{$row['name']}</option>";
