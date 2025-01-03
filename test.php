75701244c670ebf481a6c128864c16a2697d6c47
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2025-01-03 14:26:03 +0100

    .

--- test.php
+++ test.php
@@ -1,43 +1,5 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>
-      Simple Places Autocomplete - Berlin Restricted
-    </title>
-    <script type="module" src="https://ajax.googleapis.com/ajax/libs/@googlemaps/extended-component-library/0.6.11/index.min.js">
-    </script>
-    <style>
-      body {
-        padding: 25px;
-        background-color: #f0f1f3;
-        font-family: "Arial", sans-serif;
-      }
-
-      #place-picker-box {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      #place-picker-container {
-        text-align: left;
-      }
-    </style>
-  </head>
-  <body>
-    <gmpx-api-loader 
-      key="AIzaSyAeGER_6l0H6VCFt9CM1KWMMxKYAfuCiJE" 
-      solution-channel="GMP_GE_placepicker_v2">
-    </gmpx-api-loader>
-    <div id="place-picker-box">
-      <div id="place-picker-container">
-        <gmpx-place-picker 
-          placeholder="Linienstraße 128, Mitte (Adresse)" 
-          region="de" 
-          types="address" 
-          bounds="52.3382,13.0883|52.6755,13.7611">
-        </gmpx-place-picker>
-      </div>
-    </div>
-  </body>
-</html>
+<iframe src="https://storage.googleapis.com/maps-solutions-bw2xxifjos/address-selection/qr68/address-selection.html"
+  width="100%" height="100%"
+  style="border:0;"
+  loading="lazy">
+</iframe>
