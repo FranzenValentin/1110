0601163c7178487f571fd3f83957b18fc0881832
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2025-01-08 00:08:12 +0100

    .

--- index_copy.php
+++ index_copy.php
@@ -178,8 +178,8 @@ $dienstVorhanden = $dienstResult ? 1 : 0;
                                 ':stadtteil' => $stadtteil,
                                 ':fahrzeug_name' => $fahrzeug_name,
                                 ':dienst_id' => $dienst_id,
-                                ':latitude' => $latitudeEl,
-                                ':longitude' => $logitudeEl
+                                ':latitude' => document.getElementById("latitude"),
+                                ':longitude' => document.getElementById("longitude");
                             ]);
                         
                             echo "<p style='color: green;'>Einsatz wurde erfolgreich gespeichert.</p>";
