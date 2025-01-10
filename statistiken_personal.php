903b979a0e09da1c19ccbb53e1787bfe8bc4901d
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2025-01-10 08:54:19 +0100

    farben

--- statistiken_personal.php
+++ statistiken_personal.php
@@ -209,9 +209,26 @@ try {
                         datasets: [{
                             label: 'Anzahl der Einsätze',
                             data: funktionenData,
-                            backgroundColor: 'rgba(54, 162, 235, 0.5)',
-                            borderColor: 'rgba(54, 162, 235, 1)',
+                            bbackgroundColor: [
+                                'rgba(54, 162, 235, 0.5)',    // Farbe für das erste Segment
+                                'rgba(255, 99, 132, 0.5)',    // Farbe für das zweite Segment
+                                'rgba(255, 206, 86, 0.5)',    // usw.
+                                'rgba(75, 192, 192, 0.5)',
+                                'rgba(153, 102, 255, 0.5)',
+                                'rgba(255, 159, 64, 0.5)',
+                                'rgba(201, 203, 207, 0.5)'
+                            ],
+                            borderColor: [
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)',
+                                'rgba(201, 203, 207, 1)'
+                            ],
                             borderWidth: 1
+                                        
                         }]
                     },
                     options: {
