459afb2a07287ec2c8e3846af878a26bcd34a010
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2025-01-19 16:43:49 +0100

    head weg

--- session_check.php
+++ session_check.php
@@ -42,7 +42,6 @@ if (isset($_GET['check_status'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Session Check</title>
 </head>
 <body>
 <script>
