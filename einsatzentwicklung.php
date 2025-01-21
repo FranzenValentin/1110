1ca68802b434dfb0087800c218f1e8f1f59be736
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2025-01-21 13:32:02 +0100

    styles css

--- einsatzentwicklung.php
+++ einsatzentwicklung.php
@@ -74,6 +74,7 @@ $tageVorjahr = array_keys($alleTageVorjahr);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Einsatzentwicklung</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
