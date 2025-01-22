0d0907a0f10d68c8a3ffe31173fd9a58c31d301b
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2025-01-22 18:16:53 +0100

    test

--- login.php
+++ login.php
@@ -42,6 +42,7 @@ if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'] === true) {
 $error = null;
 
 echo $userID;
+echo "Test!!!!!!!";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = trim($_POST['username']);
