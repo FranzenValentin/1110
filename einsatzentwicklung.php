d356d581b72bcd7ae86e78c4aa4e87147c5fbc19
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2025-01-25 13:58:50 +0100

    .

--- einsatzentwicklung.php
+++ einsatzentwicklung.php
@@ -121,6 +121,7 @@ function createGradient(ctx, x, y, radius) {
 
 const chart = new Chart(ctx, {
     type: 'line',
+    width: 90%, // Breite des Diagramms
     data: {
         labels: tageAktuellesJahr,
         datasets: [
