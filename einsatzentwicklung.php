76dd7c2698f547ff8de3ccebab4c4873a3ecaca1
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2025-03-02 16:37:41 +0100

    .

--- einsatzentwicklung.php
+++ einsatzentwicklung.php
@@ -370,7 +370,7 @@ const chart = new Chart(ctx, {
                     display: false, // Keine eigenen Gitterlinien zeichnen
                 },
                 ticks: {
-                    stepSize: 50, // Schrittweite der Y-Achse
+                    stepSize: 15, // Schrittweite der Y-Achse
                     suggestedmax: maxRight + 10, // Maximalwert der Y-Achse
                     callback: function (value) {
                         return value; // Werte der rechten Y-Achse anzeigen
