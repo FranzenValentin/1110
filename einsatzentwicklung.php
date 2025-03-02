17d1fc2e2d69e5016299bd25135ef7e3c5231d20
FranzenValentin <96124198+FranzenValentin@users.noreply.github.com>
2025-03-02 16:44:39 +0100

    .

--- einsatzentwicklung.php
+++ einsatzentwicklung.php
@@ -362,7 +362,7 @@ const chart = new Chart(ctx, {
             'y-right': { // Rechte Y-Achse (Monatliche Einsätze)
                 position: 'right',
                 beginAtZero: true,
-                max: MaxRight, // Maximalwert der Y-Achse
+                max: maxRight, // Maximalwert der Y-Achse
                 title: {
                     display: true,
                     text: 'Monatliche Einsätze',
